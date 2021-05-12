ad504953857ba9b1b1bd00358321d7b43cfe040b
TobiasKellner <61740803+TobiasKellner@users.noreply.github.com>
2021-05-12 11:39:41 +0200

    Delete deploy.R

--- deploy.R
+++
@@ -1,6 +0,0 @@
-rsconnect::setAccountInfo(name='tobias-kellner',
-                          token='5CDBBF9FB20939E120F01CE67009E91A',
-                          secret='PQvKb6LWqusGoCFZ8lhwc5nVz+eTQOrC8HEtDEY3')
-
-
-rsconnect::deployApp('Klima_in_Deutschland.Rmd')
